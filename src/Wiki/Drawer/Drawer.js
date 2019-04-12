78e7f9f3f40d8ad87940ce62e395af2de44089fc
Michael Archer <mikearche@gmail.com>
2019-04-11 20:21:28 -0700

    Changed drawer to stateless component

--- src/Wiki/Drawer/Drawer.js
+++ src/Wiki/Drawer/Drawer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Drawer as MuiDrawer, withStyles, List } from '@material-ui/core';
 import styled from 'styled-components';
 import CategoryTabs from './CategoryTabs/CategoryTabs';
@@ -18,36 +18,23 @@ const DrawerLogoContainer = styled.div`
     padding: 35px;
 `
 
-class Drawer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            categoryOpen: 0,
-        }
-    }
-
-    togglePage = (categoryId) => {
-        this.setState({ categoryOpen: categoryId })
-    }
-
-    render() {
-        return (
-            <StyledDrawer variant={this.props.isDesktop ? "permanent" : "temporary"} open={this.props.drawerOpen} onClose={this.props}>
-                <DataContext.Consumer>
-                    {data =>
-                        <>
-                            <DrawerLogoContainer>
-                                <DrawerLogo src={data.home.logo} />
-                            </DrawerLogoContainer>
-                            <List>
-                                <CategoryTabs categories={data.categories} pages={data.pages} />
-                            </List>
-                        </>
-                    }
-                </DataContext.Consumer>
-            </StyledDrawer>
-        );
-    }
+const Drawer = (props) => {
+    return (
+        <StyledDrawer variant={props.isDesktop ? "permanent" : "temporary"} open={props.drawerOpen} onClose={props.toggleDrawer}>
+            <DataContext.Consumer>
+                {data =>
+                    <>
+                        <DrawerLogoContainer>
+                            <DrawerLogo src={data.home.logo} />
+                        </DrawerLogoContainer>
+                        <List>
+                            <CategoryTabs categories={data.categories} pages={data.pages} />
+                        </List>
+                    </>
+                }
+            </DataContext.Consumer>
+        </StyledDrawer>
+    );
 }
 
 export default Drawer;
