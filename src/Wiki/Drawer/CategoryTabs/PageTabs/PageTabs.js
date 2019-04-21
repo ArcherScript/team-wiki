43ad25d97a34539a86d525a9db042f2f3115a390
Michael Archer <mikearche@gmail.com>
2019-04-21 13:15:08 -0700

    Implement page tab redirect

--- src/Wiki/Drawer/CategoryTabs/PageTabs/PageTabs.js
+++ src/Wiki/Drawer/CategoryTabs/PageTabs/PageTabs.js
@@ -6,12 +6,16 @@ const NestedListItem = styled(ListItem)`
     padding-left: 32px !important;
 `
 
+const redirectUser = (pageTitle) => {
+    window.location = `${window.location.origin}/Wiki?page=${pageTitle}`
+}
+
 const PageTabs = (props) => {
     const { pages } = props;
     return (
         pages.map(page => {
             return (
-                <NestedListItem key={page.id} dense unit={props.theme.spacing.unit} button>
+                <NestedListItem key={page.id} onClick={() => redirectUser(page.title)} dense unit={props.theme.spacing.unit} button>
                     <ListItemText>{page.title}</ListItemText>
                 </NestedListItem>
             );
