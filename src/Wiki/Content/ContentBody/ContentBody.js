d193352808e7e8afd14acc0a0ac98cbd6573a358
Michael Archer <mikearche@gmail.com>
2019-04-28 17:54:54 -0700

    Implement text articles for pages

--- src/Wiki/Content/ContentBody/ContentBody.js
+++ src/Wiki/Content/ContentBody/ContentBody.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { Typography, Divider } from '@material-ui/core';
 import styled from 'styled-components';
+
+const ArticleTitle = styled(Typography)`
+    padding-top: 24px;
+    padding-bottom: 12px;
+`
+
+const ArticleDescription = styled(Typography)`
+    padding-bottom: 12px;
+`
 
 const ContentBody = (props) => {
     return (
-        <Typography variant="h4">
-            {props.articles[0].title}
-        </Typography>
+        <div>
+            {props.articles.map((article, index) => {
+                return (
+                    <Fragment key={article.id}>
+                        <ArticleTitle variant="h4">
+                            {article.title}
+                        </ArticleTitle>
+                        <ArticleDescription variant="body1">
+                            {article.description}
+                        </ArticleDescription>
+                        {(index + 1) !== props.articles.length && <Divider />}
+                    </Fragment>
+                );
+            })
+            }
+        </div>
     );
 };
 
